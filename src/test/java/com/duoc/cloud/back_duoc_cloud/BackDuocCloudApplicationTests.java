a024a61c00bf72c10e0752006df3c87e304ca75c
pabloperezarcos <pabloperezarcos@gmail.com>
2025-01-08 00:29:37 -0300

    Initial commit

---
+++ src/test/java/com/duoc/cloud/back_duoc_cloud/BackDuocCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.cloud.back_duoc_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDuocCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
