220971053d056993905d5ab481abab6486ae3ff8
pabloperezarcos <pabloperezarcos@gmail.com>
2025-01-09 23:16:08 -0300

    Create BackDuocCloudApplication.java

---
+++ src/main/java/proyecto/back_duoc_cloud/BackDuocCloudApplication.java
@@ -0,0 +1,13 @@
+package proyecto.back_duoc_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDuocCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackDuocCloudApplication.class, args);
+	}
+
+}
