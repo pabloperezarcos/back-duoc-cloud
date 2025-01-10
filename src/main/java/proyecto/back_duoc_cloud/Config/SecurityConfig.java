43e0cd21441ea78d95f449441bd8d103ba1ae9f8
pabloperezarcos <pabloperezarcos@gmail.com>
2025-01-10 02:13:39 -0300

    Update SecurityConfig.java
    Co-Authored-By: Byron Jaramillo <58616317+bj505@users.noreply.github.com>

--- src/main/java/proyecto/back_duoc_cloud/Config/SecurityConfig.java
+++ src/main/java/proyecto/back_duoc_cloud/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package proyecto.back_duoc_cloud.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,11 +12,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Deshabilita CSRF
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {
+                }) // Habilitar CORS
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/alertas").permitAll() // Permitir acceso a /api/alertas sin autenticación
-                        .anyRequest().authenticated() // Requiere autenticación para otras rutas
-                )
+                        .requestMatchers("/api/alertas/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(httpBasic -> {
                 }); // Configuración para autenticación básica
 
