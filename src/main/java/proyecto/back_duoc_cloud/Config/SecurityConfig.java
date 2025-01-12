a0b4a81d96963a2296ac94c107c4dd26e7cf43a7
pabloperezarcos <pabloperezarcos@gmail.com>
2025-01-12 14:06:49 -0300

    Update CORS

--- src/main/java/proyecto/back_duoc_cloud/Config/SecurityConfig.java
+++ src/main/java/proyecto/back_duoc_cloud/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -13,17 +14,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {
-                }) // Habilitar CORS
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.addAllowedOrigin("*"); // Permitir todos los orígenes
+                    config.addAllowedHeader("*"); // Permitir todos los encabezados
+                    config.addAllowedMethod("*"); // Permitir todos los métodos HTTP
+                    return config;
+                }))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/api/alertas/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated())
-                 .httpBasic(httpBasic -> { }); // Configuración para autenticación básica
-                // .httpBasic(httpBasic -> httpBasic.disable());
+                .httpBasic(httpBasic -> {
+                }); // Configuración para autenticación básica
+        // .httpBasic(httpBasic -> httpBasic.disable());
 
         return http.build();
     }
-
 
 }
